248dcc4efef312c70b376a32ac1578f4adcb087f
Tyler Griggs <tgriggs@berkeley.edu>
2025-09-07 21:58:40 +0000

    cleanup

--- skyrl-train/skyrl_train/distributed/strategy.py
+++ skyrl-train/skyrl_train/distributed/strategy.py
@@ -118,23 +118,23 @@ class DistributedStrategy(ABC):
         hf_config_tokenizer_path = os.path.join(ckpt_dir, "huggingface")
         io.makedirs(hf_config_tokenizer_path, exist_ok=True)
         model_config = model.config
-        generation_config = None
-        if model.can_generate() and hasattr(model_config, "name_or_path") and model_config.name_or_path:
-            try:
-                # Some model's name_or_path is empty if not initialized from pretrained,
-                # in this cases, we don't save generation config.
-                generation_config = GenerationConfig.from_pretrained(model_config.name_or_path)
-                with io.local_work_dir(hf_config_tokenizer_path) as work_dir:
-                    generation_config.save_pretrained(work_dir)
-            except Exception as e:
-                # if the generation config isn't available, we don't save it
-                logger.warning(f"Could not save generation config for '{model_config.name_or_path}'. Error: {e}")
-                pass
 
         with io.local_work_dir(hf_config_tokenizer_path) as work_dir:
             model_config.save_pretrained(work_dir)
             if tokenizer is not None:
                 tokenizer.save_pretrained(work_dir)
+
+            if model.can_generate() and hasattr(model_config, "name_or_path") and model_config.name_or_path:
+                try:
+                    # Some model's name_or_path is empty if not initialized from pretrained,
+                    # in this cases, we don't save generation config.
+                    generation_config = GenerationConfig.from_pretrained(model_config.name_or_path)
+                    # with io.local_work_dir(hf_config_tokenizer_path) as work_dir:
+                    generation_config.save_pretrained(work_dir)
+                except Exception as e:
+                    # if the generation config isn't available, we don't save it
+                    logger.warning(f"Could not save generation config for '{model_config.name_or_path}'. Error: {e}")
+                    pass
 
     @staticmethod
     def get_rng_state():
