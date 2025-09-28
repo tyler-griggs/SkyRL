846f23ced22f7d18be61685e869a371d8eddab4b
Tyler Griggs <tgriggs@berkeley.edu>
2025-09-28 06:16:37 +0000

    pop oai args

--- skyrl-train/skyrl_train/inference_engines/vllm/vllm_engine.py
+++ skyrl-train/skyrl_train/inference_engines/vllm/vllm_engine.py
@@ -30,7 +30,7 @@ from skyrl_train.inference_engines.base import (
     InferenceEngineOutput,
     NamedWeightsUpdateRequest,
 )
-from skyrl_train.inference_engines.vllm.utils import extract_openai_kwargs
+from skyrl_train.inference_engines.vllm.utils import pop_openai_kwargs
 from loguru import logger
 from skyrl_train.utils import str_to_torch_dtype
 
@@ -335,8 +335,8 @@ class AsyncVLLMInferenceEngine(BaseVLLMInferenceEngine):
     """Asynchronous VLLM engine."""
 
     def _create_engine(self, *args, **kwargs):
+        openai_kwargs = pop_openai_kwargs(kwargs)
         # TODO (erictang000): potentially enable log requests for a debugging mode
-        openai_kwargs = extract_openai_kwargs(kwargs)
         engine_args = vllm.AsyncEngineArgs(enable_log_requests=False, **kwargs)
         engine = vllm.AsyncLLMEngine.from_engine_args(engine_args)
 
