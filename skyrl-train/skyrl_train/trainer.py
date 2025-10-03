a1463f9e7069b0dce683559820c3b5444006619a
Tyler Griggs <131809874+tyler-griggs@users.noreply.github.com>
2025-10-03 19:46:46 -0400

    [fix] Broken import (#384)

--- skyrl-train/skyrl_train/trainer.py
+++ skyrl-train/skyrl_train/trainer.py
@@ -26,8 +26,8 @@ from skyrl_train.generators.utils import get_metrics_from_generator_output, prep
 from skyrl_train.dataset.preprocess import (
     convert_prompts_responses_to_batch_tensors,
 )
-from skyrl_train.utils import ppo_utils
-from skyrl_train.utils import trainer_utils, io
+from skyrl_train.utils import ppo_utils, trainer_utils
+from skyrl_train.utils.io import io
 from skyrl_train.utils import Timer, get_ray_pg_ready_with_timeout
 from skyrl_train.utils.constants import SKYRL_RAY_PG_TIMEOUT_IN_S
 from skyrl_train.utils.ppo_utils import (
