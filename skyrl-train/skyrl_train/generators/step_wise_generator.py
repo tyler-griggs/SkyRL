a30405f13b7345c620bd936c006616f3fcb463f9
Sumanth R Hegde <39546518+SumanthRH@users.noreply.github.com>
2025-11-24 15:42:48 -0800

    [skyrl-train][step-wise] 1/N - Support step-wise training with `step_wise_training` flag (#694)
    # What does this PR do?
    
    Supports step wise training natively with `step_wise_training`
    
    Currently step-wise training introduces a new generator output format,
    and some custom book-keeping in the agent loop. For the first
    integration, we add this functionality as a separate generator.
    
    I plan to have a follow-up PR where we simplify this and have the logic
    in the base generator.
    
    TODO:
    - [x] Migrate `step_wise` example to use the same flag
    - [x] Update docs
    - [x] Clean up trainer
    
    ---------
    
    Signed-off-by: SumanthRH <sumanthrh99@gmail.com>

--- skyrl-train/skyrl_train/generators/step_wise_generator.py
+++ skyrl-train/skyrl_train/generators/step_wise_generator.py
@@ -5,12 +5,11 @@ This file implements ``StepWiseGenerator`` for step-wise training
 import copy
 from uuid import uuid4
 import skyrl_gym
-from typing import List, Dict, Any, Optional, Union, Tuple
+from typing import List, Dict, Any, Optional, Tuple
 from tqdm.asyncio import tqdm
-from dataclasses import dataclass
 
 from skyrl_train.generators.base import GeneratorInput, GeneratorOutput, TrajectoryID
-from skyrl_train.generators.skyrl_gym_generator import SkyRLGymGenerator
+from skyrl_train.generators.skyrl_gym_generator import SkyRLGymGenerator, AgentLoopOutput
 from skyrl_train.inference_engines.inference_engine_client import InferenceEngineClient
 from skyrl_train.inference_engines.base import InferenceEngineInput, ConversationType
 from omegaconf import DictConfig
@@ -24,18 +23,6 @@ from skyrl_train.generators.utils import (
 class StepWiseGeneratorOutput(GeneratorOutput):
     trajectory_ids: List[TrajectoryID]
     is_last_step: List[bool]
-
-
-@dataclass
-class AgentLoopOutput:
-    """Output from a single agent_loop execution."""
-
-    response_ids: List[int]
-    reward: Union[List[float], float]
-    stop_reason: str
-    loss_mask: List[int]
-    prompt_ids: List[int]
-    rollout_logprobs: Optional[List[float]]
 
 
 class StepWiseGenerator(SkyRLGymGenerator):
@@ -188,6 +175,7 @@ class StepWiseGenerator(SkyRLGymGenerator):
                 prompt_ids=copy.deepcopy(input_ids[:current_prompt_length]),
                 rollout_logprobs=response_logprobs,
                 stop_reason=stop_reason,
+                env_metrics=env.get_metrics() if done else {},
             )
 
             assert len(per_step_output.loss_mask) == len(
